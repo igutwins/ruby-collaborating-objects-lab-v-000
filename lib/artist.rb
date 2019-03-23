a2f69576097e85d7c2a9c6804bbf7e5c6f5ff264
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-23 17:39:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
 attr_accessor :name
-
+@@all = []
 def initialize(name)
   @name = name
   @songs = []
@@ -9,7 +9,7 @@ def add_song(song)
   @songs << song
   song.artist = self
 end
-def songs
+def songs 
   @songs
-end
+end 
 end

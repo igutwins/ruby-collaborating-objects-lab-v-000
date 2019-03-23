3fcd1792e3897796838cd5868b93ada3eec028d8
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-23 17:54:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,28 @@
 class Artist
 attr_accessor :name
 @@all = []
-def initialize(name)
-  @name = name
-  @songs = []
-end
-def add_song(song)
-  @songs << song
-  song.artist = self
-end
-def songs
-  @songs
-end
-def save
-  self.class.all << self
-end
-def self.all
-  @@all
-end
-def find_or_create_by_name(name)
-
-end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+  def songs
+    @songs
+  end
+  def save
+    self.class.all << self
+  end
+  def self.all
+    @@all
+  end
+  def find_or_create_by_name(name)
+    if @@all.include?(name)
+      name
+    else 
+      self.new(name)
+    end 
+  end
 end
